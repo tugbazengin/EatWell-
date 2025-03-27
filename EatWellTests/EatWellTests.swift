20a9779b7107b4b7e96d87485765d50ef4845653
tugbazenginm <tugbaazngn02@gmail.com>
2025-03-27 17:53:14 +0300

    Initial Commit

---
+++ EatWellTests/EatWellTests.swift
@@ -0,0 +1,17 @@
+//
+//  EatWellTests.swift
+//  EatWellTests
+//
+//  Created by Tuğba Zengin on 27.03.2025.
+//
+
+import Testing
+@testable import EatWell
+
+struct EatWellTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
